31bd44a2b0821827632e961792ee9fc7192b9052
samanthaotazo <164980722+samanthaotazo@users.noreply.github.com>
2024-10-04 14:53:47 -0400

    Create unit_circle_intersections.c

---
+++ unit_circle_intersections.c
@@ -0,0 +1,21 @@
+/*Print the intersections of a circle with radius of user input
+ * with the x and y axis, starting from the positive y axis and moving clockwise.
+ * Created 4 October 2024 by Sam Otazo*/
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int angle = M_PI / 2;
+    int radius;
+    int count = 0;
+
+    printf("Give me a radius: \n");
+    scanf("%d", &radius);
+
+    for (count = 0; count < 4; count++){
+        printf("Our %d position is at (%.2f, %.2f)\n", count, radius * cos(angle - (M_PI/2)* count), radius * sin(angle - M_PI / 2 * count));
+}
+    return 0;
+}
