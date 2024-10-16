8e79e1a7bfcbc49c33a93d3e168f0bfe4a93226b
samanthaotazo <164980722+samanthaotazo@users.noreply.github.com>
2024-10-16 14:46:37 -0400

    Update lab_07_questions_oct16.c

--- lab_07_questions_oct16.c
+++ lab_07_questions_oct16.c
@@ -2,6 +2,8 @@
  * discussion questions.
  * Created 9 October by Samantha Otazo*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main(void) {
 /*Question 1: You are asked to write a program to read in two integers x
@@ -12,26 +14,29 @@ int main(void) {
     2. x = 2, y = -1
     3. x = -2, y = -1
     4. x = -2, y = 1*/
-    int x,y;
-    scanf("%d%d", &x, &y);
+    /*int x,y;
+    scanf("%d %d", &x, &y); //read in 2 positive integers
     if (x > 0)
         if (y > 0)
             printf("Both positive.\n");
-    else
+    else //note that this gets paired with y > 0 because of the lack of parentheses
         printf("x negative, y ignored.\n");
+    //else
+        //printf("x is negative");*/
 
 
 
 /*Question 2: What will be the output of the following code segment? Think about the answer and then check
 your answer by testing it in a complete program.*/
     int a = 5;
-    if (a = 0) {
+    if (a = 0) { //note that single equals is assignment
         printf("a=0 is true. \n");
     }
     else {
         printf("a=0 is not true.\n");
     }
-    if (a==0) {
+    //we previously set a to 0 in our first if statement
+    if (a==0) { //evaluating equality
         printf("a==0 is true. \n");
     }
     else {
@@ -40,6 +45,11 @@ your answer by testing it in a complete program.*/
 
 /*Question 3: Write a C program that generates 6 random even numbers between 2 and 20 (including both 2 and
 20) and prints them out.*/
+    //make sure to include #include <stdlib.h>
+    srand(time(NULL)); //include time.h header file for this
+    //general form of random # generation: start# + stepsize*(rand() % #of#);
+    int result = 2 + 2*(rand() % 10); 
+    printf("Our random result is %d", result);
 
     return 0;
 }
