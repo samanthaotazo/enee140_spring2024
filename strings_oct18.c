072af7bfdb8e7c4b17ecc716618138e88c75a0b9
samanthaotazo <164980722+samanthaotazo@users.noreply.github.com>
2024-10-18 13:51:24 -0400

    Create strings_oct18.c

---
+++ strings_oct18.c
@@ -0,0 +1,25 @@
+/*Write code to scan in user input and print
+ * all lowercase letters as capitals*/
+
+#include <stdio.h>
+#define MAXSIZE 30
+
+int main(void) {
+    char arr[MAXSIZE];
+    int i;
+
+    //scan in user input
+    //scanf(“%9s”, str);
+    //fgets(str, n, stdin);
+    
+    //capitalize lowercase letters only
+    for (i = 0; i < ; i++) {
+        if () {
+            arr[i] = ;
+        }
+    }
+    
+    //print the string
+    printf("%s", arr)
+    return 0;
+}
